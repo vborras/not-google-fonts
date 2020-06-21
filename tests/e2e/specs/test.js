51f23fc4e8ca0529923c7a107b21946a4711c056
Victor Borràs <vborrasc@gmail.com>
2020-06-21 10:43:51 +0200

    add e2e test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,9 @@
-// https://docs.cypress.io/api/introduction/api.html
-
-describe("My First Test", () => {
-  it("Visits the app root url", () => {
+describe("Happy path", () => {
+  it("Happy path", () => {
     cy.visit("/");
-    cy.get("#app").should("exist");
+    cy.get(".card").should("have.length", 10);
+
+    cy.get("button").click();
+    cy.get(".card").should("have.length", 20);
   });
 });
